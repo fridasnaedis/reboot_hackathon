e47a88b6eff03f05ef04b9563b28262d008f5d34
fridasnaedis <fridasnaedis@gmail.com>
2018-11-07 14:52:16 +0000

    Add copy button to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -4,15 +4,17 @@ import './footer.css';
 const baseurl = process.env.REACT_APP_SUB_URL;
 
 class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'reboothackiceland@gmail.com',
+      copied: '',
+    };
+  }
   render() {
     return (
       <div className="footer">
         <div className="footer--sponsors">
-          <div className="footer--sponsors-img">
-            <div>
-              <img src={`${baseurl}/images/ImgOne.jpg`} alt="Microsoft logo" />
-            </div>
-          </div>
           <div className="footer--sponsors-img">
             <div>
               <img
@@ -23,7 +25,26 @@ class Footer extends Component {
           </div>
         </div>
         <div className="footer--info">
-          <p>reboothackiceland@gmail.com</p>
+          <div className="footer--email">
+            <p>reboothackiceland@gmail.com</p>
+            {/* Logical shortcut for only displaying the 
+            button if the copy command exists */
+            'clipboard' in navigator && (
+              <div>
+                <button
+                  onClick={() => {
+                    navigator.clipboard.writeText(this.state.email);
+                    this.setState(state => ({
+                      copied: 'Copied!',
+                    }));
+                  }}
+                >
+                  Copy email
+                </button>
+              </div>
+            )}
+          </div>
+          <div>{this.state.copied}</div>
           <a
             href="https://github.com/KetillG/simple_hackathon"
             rel="noopener noreferrer"
